9ee7e5c13c6ed20f4ea803a8d2177ca0d6d6166d
vvvvvv111111 <vr-nic@mail.ru>
2026-02-04 10:43:32 +0300

    .

--- university/firstHomeWork.py
+++ university/firstHomeWork.py
@@ -4,3 +4,14 @@ def calculate_bmi(weight, height):
     category = ("Недостаточный вес", "Нормальный вес", "Избыточный вес", "Ожирение")[(BMI < 18.5)+(BMI < 25) + (BMI < 30)]
     return (BMI, category)
 print(calculate_bmi(67, 67))
+
+print('\n')
+
+def analyse_text(text):
+    diff = text.split(' ')
+    print('Длина строки' ,len(diff))
+    print('Колличество символов' , len(text.replace(" ", "")))
+    print('максимальная длина слова' , max(diff, key=len))
+    print('средняя длина строки' , len(text.replace(" ", "")) / len(diff))
+    print('колличество букв в верхнем регистре' , len([count for count in text if count.isupper()]))
+print(analyse_text('vjif jFFFFidf jdkf'))
