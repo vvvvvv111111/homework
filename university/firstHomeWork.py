0a7dc35218a038d0957df826d58460cd0624ff8a
vvvvvv111111 <vr-nic@mail.ru>
2026-02-06 18:57:17 +0300

    .

--- university/firstHomeWork.py
+++ university/firstHomeWork.py
@@ -17,3 +17,46 @@ def analyse_text(text):
     print('средняя длина строки' , len(text.replace(" ", "")) / len(diff))
     print('колличество букв в верхнем регистре' , len([count for count in text if count.isupper()]))
 print(analyse_text('vjif jFFFFidf jdkf'))
+
+# генератор безопасных паролей
+def generate_password(lenght, use_special=True):
+    import random
+    import secrets
+    import string
+
+    password=[]
+    password = [secrets.choice(string.ascii_lowercase) for a in range(2)] + [secrets.choice(string.ascii_uppercase) for a in range(2)]
+    password += [secrets.choice(string.digits) for a in range(2)]
+    if use_special == True:
+        password +=  [secrets.choice("!@#$%^&*") for a in range(2)]
+        nedostatoch= lenght - 8
+        dopchisla= ''.join(secrets.choice(string.ascii_letters + string.digits + "!@#$%^&*") for _ in range(nedostatoch))
+    else:
+        nedostatoch= lenght - 6
+        dopchisla= ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(nedostatoch))
+
+    password += dopchisla
+
+    secrets.SystemRandom().shuffle(password)
+
+
+    digits = any( i.isdigit() for i in password )
+    letter = any( i.isascii() for i in password )
+    speeec= "!@#$%^&*"
+    special = any(i in speeec for i in password)
+    svag = []
+    if letter == True: 
+        category = "слабый"
+    if all([digits, letter]):
+        category = "средний"
+    if all([digits, letter,special]):
+        category = "хороший"
+    if (all([digits, letter,special])) and lenght > 14:
+        category = "отличный"
+    if (all([digits, letter,special])) and lenght > 16:
+        category = "превосходный"
+    
+    return(password,category)
+
+print(generate_password(17, True))
+    
